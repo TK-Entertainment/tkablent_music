bdf56b04f5d4a216f1a9d3b1008d982144feba6c
Kevin Chang <kevin0216@kevin0216.us>
2024-11-04 14:59:31 +0800

    [Core] Bumping version m.20240318.5.p7-s

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if production:
     status = discord.Status.online
     production_status = "s"  # ce for cutting edge, s for stable
     test_subject = "wl3.0_test"
-    bot_version = "m.20240318.5.p5{}-{}".format(f".{test_subject}" if production_status != "s" else "", production_status)
+    bot_version = "m.20240318.5.p7{}-{}".format(f".{test_subject}" if production_status != "s" else "", production_status)
 else:
     status = discord.Status.dnd
     bot_version = f"LOCAL DEVELOPMENT / {branch} Branch\nMusic Function"
@@ -49,7 +49,7 @@ async def precense_update():
         for precense in precenses:
             try:
                 await bot.change_presence(activity=precense)
-            except discord.errors.HTTPException:
+            except Exception:
                 pass
             await asyncio.sleep(10)
 
